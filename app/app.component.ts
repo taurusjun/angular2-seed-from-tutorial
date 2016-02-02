333298b6076cfa04a8631f3a87225603c6f71090
Wang Jun <taurusjun@126.com>
2016-02-02 17:46:08 +0800

    upload seed files

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by I306067 on 1/25/2016.
+ */
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App dssdss</h1>'
+})
+export class AppComponent { }
